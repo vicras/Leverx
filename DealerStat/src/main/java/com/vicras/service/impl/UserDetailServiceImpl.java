a51b44f37174de15daac268dc6ec2898288aec68
vicras <victor.graskov@gmail.com>
2021-04-20 12:27:34 +0300

    fix: cyclic bean reference

---
+++ DealerStat/src/main/java/com/vicras/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,53 @@
+package com.vicras.service.impl;
+
+import com.vicras.entity.EntityStatus;
+import com.vicras.entity.User;
+import com.vicras.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        boolean accountNotExpired = true;
+        boolean credentialsNotExpired = true;
+        boolean accountNotLocked = true;
+
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        User user = optionalUser.orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User '%s' not found", email)));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                isUserActive(user),
+                accountNotExpired,
+                credentialsNotExpired,
+                accountNotLocked,
+                getUserCredentials(user)
+        );
+    }
+
+    private boolean isUserActive(User user) {
+        return user.getEntityStatus() == EntityStatus.ACTIVE;
+    }
+
+    private Collection<? extends GrantedAuthority> getUserCredentials(User user) {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+}
