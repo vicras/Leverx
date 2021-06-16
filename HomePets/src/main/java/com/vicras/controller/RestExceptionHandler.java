07a2545c53c82e2de915f00bdca025ce0efc762e
vicras <victor.graskov@gmail.com>
2021-06-16 16:59:56 +0300

    feat: validation message

--- HomePets/src/main/java/com/vicras/controller/RestExceptionHandler.java
+++ HomePets/src/main/java/com/vicras/controller/RestExceptionHandler.java
@@ -1,20 +1,45 @@
 package com.vicras.controller;
 
 import com.vicras.exception.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+@Slf4j
+@ControllerAdvice()
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        log.info(ex.getMessage());
         return handleExceptionInternal(ex, ex.getMessage(),
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        StringBuilder message = new StringBuilder("Missing required fields contracts:");
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            message
+                    .append(" [")
+                    .append(error.getField())
+                    .append(" : ")
+                    .append(error.getDefaultMessage())
+                    .append("]");
+        }
+        log.info(message.toString());
+        return new ResponseEntity<>(message.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
 }
